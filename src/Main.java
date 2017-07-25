4d963333dd61531d2b5c5b0be1edef97ec9b9462
bauGid <fun-man2000@web.de>
2017-07-26 01:01:23 +0200

    parallelised the calculation

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,21 @@ public class Main {
         }
     }
 
+
     public static ArrayList<Pair> genFriendNumbsToMax(long max) {
         ArrayList<Pair> out = new ArrayList<>();
-        long dividerSum;
-        for (long i = 2; i < max; i++) {
-            dividerSum = sumDivider(i);
+        LongStream.range(2, max).parallel().forEach((i) -> {
+            long dividerSum = sumDivider(i);
             if (i != dividerSum && i < dividerSum && i == sumDivider(dividerSum)) {
-                out.add(new Pair(i, dividerSum));
+                synchronized (out) {
+                    out.add(new Pair(i, dividerSum));
+                }
             }
-        }
+        });
         return out;
     }
 
     public static long sumDivider(long n) {
-
         HashSet<Long> lowerSet = LongStream.rangeClosed(2, (long) Math.sqrt(n)).filter((i) -> n % i == 0).boxed().collect(Collectors.toCollection(HashSet::new));  //find all divider of n below sqrt(n) and convert to a HashSet
         long totalSum = lowerSet.stream().reduce(0L, Long::sum) + 1; // sum them up
         for (long l : lowerSet) { //calculate and sum up the dividers  of n greater than sqrt(n)
@@ -46,10 +47,16 @@ public class Main {
             error = false;
             max = input.nextLong();
             if (max <= 1) {
-                System.out.println("Maximum value must be greater than One!");
+                System.out.println("Maximum value must be greater than one!");
                 error = true;
             }
         } while (error);
-        genFriendNumbsToMax(max).forEach((element) -> System.out.println(element.number1 + ", " + element.number2));
+        long begin = System.nanoTime();
+        ArrayList<Pair> result = genFriendNumbsToMax(max);
+        System.out.println("Done!\nSorting...");
+        result.stream()
+                .sorted(Comparator.comparingLong(o -> o.number1)) //compares the numbers
+                .forEach((element) -> System.out.println(element.number1 + ", " + element.number2));
+        System.out.println(System.nanoTime()-begin);
     }
 }
